e81b664d310bf0fce8526f48a6d9bf627e28185a
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-01-29 14:46:29 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ import { renderSummarySale } from "./summary/summarySale.js";
 import { renderSummaryStock } from "./summary/summaryStock.js";
 import { renderSummarySCBand } from "./summary/summarySCBand.js";
 import { renderSummarySize } from "./summary/summarySize.js";
+import { renderSummaryRemark } from "./summary/summaryRemark.js";
 
 async function initApp() {
   try {
@@ -18,10 +19,11 @@ async function initApp() {
     // -------------------------------
     // Render Summaries
     // -------------------------------
-    renderSummarySale(data);     // Summary 1
-    renderSummaryStock(data);    // Summary 2
-    renderSummarySCBand(data);   // Summary 3
-    renderSummarySize(data);     // Summary 4
+    renderSummarySale(data);      // Summary 1
+    renderSummaryStock(data);     // Summary 2
+    renderSummarySCBand(data);    // Summary 3
+    renderSummarySize(data);      // Summary 4
+    renderSummaryRemark(data);    // Summary 5
 
     console.log("🚀 Demand Planning App Ready");
   } catch (error) {
